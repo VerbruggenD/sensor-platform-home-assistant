c912fc8b01ab07a090688178dea621a0d4fcab42
VerbruggenD <verbruggen.dieter@gmail.com>
2025-10-10 18:27:58 +0200

    restructured misc tools and removed unused scripts

---
+++ pico-firmware/install/credentials-template.py
@@ -0,0 +1,9 @@
+############################################
+##### SENSOR PLATFORM - HOME ASSISTANT #####
+############################################
+# Developer: Dieter Verbruggen
+# File: credentials-template.py
+
+## WIFI CREDENTIALS ##
+wifi_ssid = "--your-wifi-ssid--"
+wifi_password = "--your-wifi-password--"
